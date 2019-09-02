5f5cb0c1c9a240872b6129991172fbe031ef4519
kmurata798 <kmurata798@gmail.com>
2019-09-02 15:37:52 -0700

    2nd commit

--- madlibs.py
+++ madlibs.py
@@ -1,18 +1,40 @@
 def madlibs():
     wordlist = list()
-    animal = input("Write an animal: ")
-    wordlist.append(animal)
-    food = input("Write a food: ")
-    wordlist.append(food)
-    adjective = input("Write a adjective: ")
-    wordlist.append(adjective)
-    number = input("Write a number: ")
-    wordlist.append(number)
+    
+    noun = input("Write a noun: ")
+    wordlist.append(noun)
+    
     verb = input("Write a verb: ")
     wordlist.append(verb)
-    noun = input("Write an input: ")
+
+    noun2 = input("Write another noun: ")
     wordlist.append(noun)
 
-    print("You see a {} eating {} outside of your window. You open the window and snatch the food {} and eat it. After you finish, you say {} and take a nap.".format(animal, food, adjective, number))
+    adjective = input("Write a adjective: ")
+    wordlist.append(adjective)
+    
+    verb2= input("Write another verb: ")
+    wordlist.append(verb2)
+                    
+    adjective2 = input("Write another adjective: ")
+    wordlist.append(adjective2)
+    
+    noun3 = input("Write one more noun: ")
+    wordlist.append(noun3)
+    
+    animal = input("Write an animal: ")
+    wordlist.append(animal)
+    
+    food = input("Write a food: ")
+    wordlist.append(food)
+    
+    number = input("Write a number: ")
+    wordlist.append(number)
+
+    
+    print("\nOnce there was a little {} that would always {} next of a {}.\nThe weather was {} so there was a party where everyone had to {} until the party was over".format(noun, verb, noun2, adjective, verb2))
+    print("There was all kinds of {} food at the party, but the {} was the most popular, everyone was eating that.".format(adjective2, food))
+    print("Right before the party ended, a {} came down from the ceiling and sounded like a {}.\nIt sat down and had a drink, and all of a sudden {} more of those things joined the party and everyone danced all night!".format(noun3, animal, number))
+    
 
 madlibs()
