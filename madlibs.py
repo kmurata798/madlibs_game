cdfc1c3781f48f2e830137fabc70d033002ea5b7
kmurata798 <kmurata798@gmail.com>
2019-09-02 23:59:05 -0700

    modified colors

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,3 @@
-from colors import red, green, blue, yellow
-
 #Stretch 1
 wordlist = list()
 
@@ -9,7 +7,7 @@ def madlibs():
 
     if word == 'r' or word == 'R'
         for dList in wordlist:
-            print(dList)"""
+            print(wordlist(dList))"""
 
     noun = input("Write a noun: ")
     wordlist.append(noun)
@@ -44,10 +42,10 @@ def madlibs():
     number = input("Write a number: ")
     wordlist.append(number)
 
-    
-    print("\nOnce there was a little {} that would always {} next of a {}.\nThe weather was {} so there was a party where everyone had to {} until the party was over".format(red(noun), green(verb), red(noun2), yellow(adjective), green(verb2)))
-    print("There was all kinds of {} food at the party, but the {} was the most popular, everyone was eating that.".format(yellow(adjective2), orange(food)))
-    print("Right before the party ended, a {} came down from the ceiling and sounded like a {}.\nIt sat down and had a drink, and all of a sudden {} more of those things joined the party and everyone danced all night!".format(red(noun3), blue(animal), pink(number)))
-    
+    #Stretch 2 Color and Formatting
+    print("\nOnce there was a little \033[0;31m {} \033[0m that would always \033[0;32m {} \033[0m next of a \033[1;33m {} \033[0m.\nThe weather was \033[0;31m {} \033[0m so there was a party where everyone had to \033[0;32m {} \033[0m until the party was over".format(noun, verb, noun2, adjective, verb2))
+    print("There was all kinds of \033[1;33m {} \033[0m food at the party, but the \033[1;31m {} \033[0m was the most popular, everyone was eating that.".format(adjective2, food))
+    print("Right before the party ended, a \033[0;31m {} \033[0m came down from the ceiling and sounded like a \033[0;34m {} \033[0m.\nIt sat down and had a drink, and all of a sudden \033[1;35m {} \033[0m more of those things joined the party and everyone danced all night!".format(noun3, animal, number))
+    # RED \033[0;31m  GREEN = "\033[0;32m"  BLUE = "\033[0;34m"  PURPLE = "\033[0;35m"  YELLOW = "\033[1;33m"  LIGHT_RED = "\033[1;31m"  LIGHT_PURPLE = "\033[1;35m"  END = "\033[0m"
 
 madlibs()
